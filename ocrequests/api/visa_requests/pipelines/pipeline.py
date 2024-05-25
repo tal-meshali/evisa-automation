4e6d992edd653929554771087b1028b935f21cce
Tal Meshali <meshalital@gmail.com>
2024-05-25 23:34:57 +0300

    MultiStagePipeline addresses pipelines as additional stages

--- ocrequests/api/visa_requests/pipelines/pipeline.py
+++ ocrequests/api/visa_requests/pipelines/pipeline.py
@@ -1,10 +1,11 @@
 from ..stages.stage import Stage
-from typing import List
+from typing import List, Dict
 from abc import ABC
 
 
 class Pipeline(ABC):
     stages: List[Stage]
+    name = None
 
     def activate(self, initial_input):
         if len(self.stages) == 0:
@@ -13,3 +14,30 @@ class Pipeline(ABC):
         for stage in self.stages[1:]:
             output = stage.loop(output) if stage.should_loop else stage.run(output)
         return output
+
+
+class MultiStagePipeline(Pipeline):
+    pipelines: List[Pipeline]
+
+    def __init__(self, should_duplicate: bool):
+        self.should_duplicate = should_duplicate
+
+    def activate(self, initial_input):
+        pipeline_inputs = super().activate(initial_input)
+        outputs = {}
+        if self.should_duplicate:
+            for pipeline in self.pipelines:
+                self.update(outputs, pipeline, pipeline_inputs)
+        else:
+            if len(pipeline_inputs) != len(self.pipelines):
+                raise ValueError("Output amount should match pipeline amount!")
+            for pipeline_input, pipeline in zip(pipeline_inputs, self.pipelines):
+                self.update(outputs, pipeline, pipeline_input)
+        return outputs
+
+    def update(self, update_into: Dict, pipeline: Pipeline, pipeline_input):
+        pipeline_result = pipeline.activate(pipeline_input)
+        if isinstance(pipeline_result, dict):
+            update_into.update(**pipeline_result)
+        else:
+            update_into[pipeline.name] = pipeline_result
