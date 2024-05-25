fc38fc2abe2ab0efc5fa7e01566da724bde4d649
Tal Meshali <meshalital@gmail.com>
2024-05-25 23:35:14 +0300

    A new stage for recreating a valid django entity for an image

--- ocrequests/api/visa_requests/stages/image.py
+++ ocrequests/api/visa_requests/stages/image.py
@@ -1,3 +1,6 @@
+from io import BytesIO
+from sys import getsizeof
+
 from ..stages.stage import Stage
 from google.cloud import vision
 from PIL import Image
@@ -17,18 +20,18 @@ class VisionStage(Stage, ABC):
 class ConvertToVisionImageStage(Stage):
     def run(self, stage_input: InMemoryUploadedFile):
         content = stage_input.read()
-        return vision.Image(content=content)
+        return stage_input, vision.Image(content=content)
 
 
 class FaceDetectionStage(VisionStage):
     def run(self, stage_input):
-        return stage_input, self.client.face_detection(image=stage_input, max_results=1).face_annotations[
+        return stage_input[0], self.client.face_detection(image=stage_input[1], max_results=1).face_annotations[
             0]  # pylint: disable=no-member
 
 
 class PassportTextDetectionStage(VisionStage):
     def run(self, stage_input):
-        return stage_input, self.client.document_text_detection(image=stage_input).full_text_annotation
+        return self.client.document_text_detection(image=stage_input[1]).full_text_annotation
 
 
 class CropImageStage(Stage):
@@ -46,15 +49,24 @@ class CropImageStage(Stage):
             diff = ((y2 - y1) * 0.75 - (x2 - x1)) / 2
             x1 -= diff
             x2 += diff
-            return pil_image.crop((max(0, x1), y1, min(width, x2), y2))
+            return image, pil_image.crop((max(0, x1), y1, min(width, x2), y2))
         raise IOError("Image file isn't valid!")
+
+
+class SaveImageValidFormatStage(Stage):
+    def run(self, stage_input):
+        original_image, cropped_image = stage_input
+        buffer = BytesIO()
+        cropped_image.save(fp=buffer, format=original_image.content_type.split('/')[1].upper())
+        buffer.seek(0)
+        return InMemoryUploadedFile(buffer, 'ImageField', original_image.name, 'image/jpeg',
+                                            getsizeof(buffer), None)
 
 
 class ReceiveWordListStage(Stage):
     def run(self, stage_input):
-        image, detection = stage_input
         word_list = []
-        for page in detection.pages:
+        for page in stage_input.pages:
             for block in page.blocks:
                 for paragraph in block.paragraphs:
                     for word in paragraph.words:
@@ -62,4 +74,4 @@ class ReceiveWordListStage(Stage):
                         for symbol in word.symbols:
                             word_key += symbol.text
                         word_list.append(word_key)
-        return image, word_list
+        return word_list
