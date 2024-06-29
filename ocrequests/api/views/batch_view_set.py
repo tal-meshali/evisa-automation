12bc58ea825399d1b6e6f11d2130cf1d2e772aee
Tal Meshali <meshalital@gmail.com>
2024-06-29 14:37:44 +0300

    Batch is in charge of managing all related beneficiaries data

---
+++ ocrequests/api/views/batch_view_set.py
@@ -0,0 +1,29 @@
+from django.forms import model_to_dict
+from rest_framework import viewsets
+from rest_framework.response import Response
+
+from ..models import Batch, Beneficiary
+from rest_framework.decorators import action
+
+from ..serializers.batch_serializer import BatchSerializer
+
+
+class BatchViewSet(viewsets.ModelViewSet):
+    queryset = Batch.objects.all()
+    serializer_class = BatchSerializer
+
+    @action(methods=["POST"], detail="Append a beneficiary to the batch's list")
+    def create_beneficiary(self, request):
+        beneficiary = Beneficiary.objects.create()
+        beneficiary.related_batch = self.get_object()
+        beneficiary.save()
+        return Response({"created": model_to_dict(beneficiary)})
+
+    @action(
+        methods=["GET"], detail="Get all of the beneficiaries attached to the batch"
+    )
+    def get_beneficiaries(self, request):
+        beneficiaries = self.get_object().beneficiary_set.all()
+        return Response(
+            {"received": [model_to_dict(beneficiary) for beneficiary in beneficiaries]}
+        )
