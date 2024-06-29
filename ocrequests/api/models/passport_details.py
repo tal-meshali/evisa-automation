944bad5d078c96f37298aa1ea6c5865e1872c606
Tal Meshali <meshalital@gmail.com>
2024-06-29 16:12:12 +0300

    Removed upload urls dependent on pre-existing beneficiary link

--- ocrequests/api/models/passport_details.py
+++ ocrequests/api/models/passport_details.py
@@ -1,10 +1,6 @@
 from django.db import models
 
 
-def passport_image_directory_path(instance, filename):
-    return f"{instance.email_address}/passport/{filename}"
-
-
 class PassportDetails(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
@@ -13,4 +9,4 @@ class PassportDetails(models.Model):
     birth_date = models.DateField(max_length=10)
     issue_date = models.DateField(max_length=10)
     expiry_date = models.DateField(max_length=10)
-    passport_image = models.ImageField(upload_to=passport_image_directory_path)
+    passport_image = models.ImageField()
--- ocrequests/api/models/portrait_details.py
+++ ocrequests/api/models/portrait_details.py
@@ -1,9 +1,5 @@
 from django.db import models
 
 
-def portrait_image_directory_path(instance, filename):
-    return f"{instance.email_address}/portrait/{filename}"
-
-
 class PortraitDetails(models.Model):
-    portrait_image = models.ImageField(upload_to=portrait_image_directory_path)
+    portrait_image = models.ImageField()
