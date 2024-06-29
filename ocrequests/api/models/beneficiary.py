1cbb31e66200e63bd5d4e14f2e89023e442094f5
Tal Meshali <meshalital@gmail.com>
2024-06-29 16:12:30 +0300

    Saving and creating relation on create

--- ocrequests/api/models/beneficiary.py
+++ ocrequests/api/models/beneficiary.py
@@ -3,7 +3,16 @@ from ..models import PassportDetails, PortraitDetails, Batch
 
 
 class Beneficiary(models.Model):
-    passport = models.ForeignKey(PassportDetails, on_delete=models.CASCADE, null=True)
-    portrait = models.ForeignKey(PortraitDetails, on_delete=models.CASCADE, null=True)
+    passport = models.OneToOneField(
+        PassportDetails, on_delete=models.CASCADE, null=True
+    )
+    portrait = models.OneToOneField(
+        PortraitDetails, on_delete=models.CASCADE, null=True
+    )
     full_name = models.CharField(max_length=30, null=True)
     related_batch = models.ForeignKey(Batch, on_delete=models.SET_NULL, null=True)
+
+    def save_item(self, item: models.Model):
+        item.beneficiary = self
+        self.save()
+        item.save()
--- ocrequests/api/serializers/portrait_serializer.py
+++ ocrequests/api/serializers/portrait_serializer.py
@@ -1,26 +1,24 @@
 from rest_framework import serializers
-from ..models import PortraitDetails
+from ..models import PortraitDetails, Beneficiary
 from ..visa_requests.pipelines.image import PortraitImagePipeline
 from google.cloud.vision import ImageAnnotatorClient
 
 
 class PortraitSerializer(serializers.Serializer):
-    email_address = serializers.EmailField()
     portrait_image = serializers.ImageField()
     created = serializers.DateTimeField(required=False)
     client = ImageAnnotatorClient()
     pipeline = PortraitImagePipeline(client)
 
     def create(self, validated_data):
-        return PortraitDetails.objects.create(
-            email_address=validated_data["email_address"],
+        portrait = PortraitDetails.objects.create(
             portrait_image=self.pipeline.activate(validated_data["portrait_image"]),
         )
+        portrait.save()
+        Beneficiary.objects.get(pk=validated_data["beneficiary"]).save_item(portrait)
+        return portrait
 
     def update(self, instance, validated_data):
-        instance.email_address = validated_data.get(
-            "email_address", instance.email_address
-        )
         instance.portrait_image = self.pipeline.activate(
             validated_data.get("portrait_image", instance.portrait_image)
         )
